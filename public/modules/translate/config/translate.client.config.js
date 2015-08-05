3a42fbead45883f99bf9bc82c82bf91a0154806d
Hector Gonzalez <hector.gonzalez@enova.mx>
2015-08-05 09:49:55 -0500

    translate item form

--- public/modules/translate/config/translate.client.config.js
+++ public/modules/translate/config/translate.client.config.js
@@ -18,7 +18,20 @@ angular.module('translate', ['pascalprecht.translate'] ).config(function($transl
         ITEM_DESCRIPTION: 'Description',
         ITEM_THIS_FIELD_IS_INVALID: 'This field is invalid!',
         ITEM_THIS_FIELD_IS_NOT_NUMBER: 'Not valid number!',
-        ITEM_FIELD_MAX_LENGTH: 'Max Length!'
+        ITEM_FIELD_MAX_LENGTH: 'Max Length!',
+        ITEM_NEW: 'New Item',
+        ITEM_NAME_LABEL : 'Name',
+        ITEM_NAME_PLACEHOLDER : 'Ej: Mascara del Santo',
+        ITEM_PRICE_LABEL: 'Price',
+        ITEM_PRICE_PLACEHOLDER: 'Ej: 10.00',
+        ITEM_SKU_LABEL: 'SKU',
+        ITEM_SKU_PLACEHOLDER: 'Ej: LTAFD',
+        ITEM_WEIGHT_LABEL: 'Weight (Kg)',
+        ITEM_WEIGHT_PLACEHOLDER: 'Ej: 0.5',
+        ITEM_DESCRIPTION_LABEL: 'Description',
+        ITEM_DESCRIPTION_PLACEHOLDER: 'Write your Description',
+        ITEM_SEND_BUTTON: 'Send Item'
+
     });
 
     $translateProvider.translations('es', {
@@ -36,7 +49,8 @@ angular.module('translate', ['pascalprecht.translate'] ).config(function($transl
         ITEM_DESCRIPTION: 'Descripción',
         ITEM_THIS_FIELD_IS_INVALID: 'Este campo no es válido!',
         ITEM_THIS_FIELD_IS_NOT_NUMBER: 'Este campo no es numérico!',
-        ITEM_FIELD_MAX_LENGTH: 'Max Length!'
+        ITEM_FIELD_MAX_LENGTH: 'Max Length!',
+        ITEM_NEW: 'Nuevo artículo'
     });
 
     $translateProvider.preferredLanguage('en');
